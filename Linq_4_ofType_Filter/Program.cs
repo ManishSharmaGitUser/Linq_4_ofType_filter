143f41132139ce6cd0f84b45981c9d8a44cf641e
manish <manish.7151.sharma@gmail.com>
2022-06-28 00:55:32 +0530

    Add project files.

---
+++ Linq_4_ofType_Filter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_4_ofType_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Object> demodata = new List<object>() { "Manish", "Kumar", "Sharma", "JO", 1, 4, 7 };
+            var use_of_oftype_filter = demodata.OfType<string>().ToList();
+
+            var use_of_oftype_filter_with_where = demodata.OfType<string>().Where(x=>x.Length > 3).ToList();
+
+
+            foreach (var item in use_of_oftype_filter_with_where)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
